ac06155b54c106a498a4ce4f79f8bdb577f1a456
Logan Wendholt <lwendholt@fb.com>
2021-04-13 19:31:00 -0700

    Add Phabricator comment with upload results
    Summary: Show the replay commands associated with all the captured JIT test recordings under the "Other" row within SignalBox.
    
    Reviewed By: jbower-fb
    
    Differential Revision: D27711434
    
    fbshipit-source-id: c1807bff53

--- Tools/scripts/fdb_upload.py
+++ Tools/scripts/fdb_upload.py
@@ -7,11 +7,18 @@ import tempfile
 from typing import List
 
 def upload_recording(recording_path: str, tests: List[str]) -> str:
-    output_file = tempfile.NamedTemporaryFile()
-    subprocess.run(["fdb", "replay", "share", "--json-output-file", output_file.name, recording_path])
-    output = json.load(output_file)
-    key = output["key"]
-    return key
+    try:
+        output_file = tempfile.NamedTemporaryFile()
+        subprocess.run(
+            ["fdb", "replay", "share", "--json-output-file", output_file.name, recording_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        output = json.load(output_file)
+        key = output["key"]
+        return key
+    except:
+        return ""
 
 def upload_recordings_and_show_results(recording_metadata_file: str) -> None:
     with open(recording_metadata_file, "r") as f:
@@ -21,14 +28,15 @@ def upload_recordings_and_show_results(recording_metadata_file: str) -> None:
         tests = recording["tests"]
         recording_path = recording["recording_path"]
 
-        print()
-        print(f"Uploading recording for tests: {tests}")
-
         key = upload_recording(recording_path, tests)
 
-        print("Upload complete.")
-        print("To fetch and replay this recording, use the following command:")
-        print(f"  fdb replay debug {key}") 
+        if key:
+            print(f"Recording captured for tests: {tests}")
+            print("To fetch and replay this recording, use the following command:")
+            print(f"  fdb replay debug {key}")
+        else:
+            print(f"ERROR: failed to upload recording for tests: {tests}")
+        print()
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Upload recordings captured by fdb")
