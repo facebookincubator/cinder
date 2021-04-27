89374de5fab65c740cabc8c5f6492e0f412d57be
Jacob Bower <jbower@fb.com>
2021-04-27 09:41:54 -0700

    Make "configure + make" with no args work
    Summary:
    We'll always need tweaks for an optimal FB-internal build, but anything that's *needed* for a functional build at all should be in `configure`. So here we move some things from `configure_with_fb_toolchain.py` to the main `configure` script such that the OSS build can configure + make with defaults. This both reduces the risk of divergence between the OSS and internal builds, and will help avoid tripping up external users.
    
    Specific changes:
    * Always build with a C++ `main()` as we can't do without.
    * Always search from the source root for includes as this is needed for build.
    * Always apply `-fno-omit-frame-pointer` as I think we'll need this when we support no-frame mode. Also putting in `-momit-leaf-frame-pointer` as this counter-balances some of the perf loss (see discussion at  https://fburl.com/7q07pwoy) and I don't think it'll affect no-frame
      mode.
    * Add appropriate warning filters as the build will fail without these.
    
    Reviewed By: carljm
    
    Differential Revision: D28022437
    
    fbshipit-source-id: 4370704f1e

--- oss-build-and-test.sh
+++ oss-build-and-test.sh
@@ -2,19 +2,13 @@
 
 set -xe
 
-SRC_ROOT="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
-export CC=gcc
-export CXX=g++
-export LDSHARED="gcc -shared"
-export CFLAGS="-I\"$SRC_ROOT\" -fno-omit-frame-pointer -momit-leaf-frame-pointer -Wp,-D_FORTIFY_SOURCE=2 -Wno-cast-function-type -Wno-type-limits"
-export CPPFLAGS="-I\"$SRC_ROOT\" -Wno-cast-function-type -Wno-type-limits"
-export CXXFLAGS="-I\"$SRC_ROOT\" -fno-omit-frame-pointer -momit-leaf-frame-pointer -Wp,-D_FORTIFY_SOURCE=2"
-
 env
+
+SRC_ROOT="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
 mkdir build
 cd build
-"$SRC_ROOT/configure" --with-cxx-main
+"$SRC_ROOT/configure"
 make -j 4 VERBOSE=1 testcinder_jit
 make -j 4 VERBOSE=1 testruntime
 make -j 4 VERBOSE=1 testcinder
