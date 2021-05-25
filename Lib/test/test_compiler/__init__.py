3199c6dcd7aec62f599f6e654b593d4dc37c3fc3
Matthias Braun <matthiasb@fb.com>
2021-05-25 07:46:14 -0700

    Skip side-by-side compiler tests in pyro
    Summary: Skip compiler side-by-side testing in Pyro runtime. `builtins.compile` is just using the `compiler` module there anyway. This also avoids problems with some of the tests like `Python38Tests` using `py_compile` in non-pyro module producing mismatches with `builtins.compile`.
    
    Reviewed By: czardoz, wmeehan
    
    Differential Revision: D28621558
    
    fbshipit-source-id: 25f10c7f2b

--- Lib/test/test_compiler/__init__.py
+++ Lib/test/test_compiler/__init__.py
@@ -2,16 +2,23 @@ import sys
 
 from .test_api import ApiTests
 from .test_code_sbs import CodeTests
-from .test_corpus import SbsCorpusCompileTests
 from .test_errors import ErrorTests, ErrorTestsBuiltin
 from .test_flags import FlagTests
 from .test_graph import GraphTests
 from .test_optimizer import AstOptimizerTests
 from .test_peephole import PeepHoleTests
-from .test_sbs_stdlib import SbsCompileTests
 from .test_symbols import SymbolVisitorTests
 from .test_unparse import UnparseTests
 from .test_visitor import VisitorTests
+
+# Skip side-by-side tests on Pyro where builtin.compile is just using the
+# `compiler` module anyway.
+if sys.implementation.name != "pyro":
+    from .test_corpus import SbsCorpusCompileTests
+    from .test_sbs_stdlib import SbsCompileTests
+
+    if sys.version_info >= (3, 8):
+        from .test_py38 import Python38Tests
 
 try:
     import cinder
@@ -23,6 +30,3 @@ except ImportError:
 
 if sys.version_info >= (3, 7):
     from .test_py37 import Python37Tests
-
-if sys.version_info >= (3, 8):
-    from .test_py38 import Python38Tests
