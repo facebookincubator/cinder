c44e24780797c91ba9294db08d5e3f375e5ea70c
Aniket Panse <aniketpanse@fb.com>
2021-08-19 15:26:14 -0700

    Import missing test file
    Summary: This needs to be imported in order to run the tests as a suite.
    
    Reviewed By: DinoV
    
    Differential Revision: D30435727
    
    fbshipit-source-id: afc8153e48

--- Lib/test/test_compiler/test_static/__init__.py
+++ Lib/test/test_compiler/test_static/__init__.py
@@ -3,3 +3,4 @@ from .elide_type_checks import ElideTypeChecksTests
 from .obj_creation import StaticObjCreationTests
 from .property import PropertyTests
 from .tests import StaticCompilationTests, StaticRuntimeTests
+from .walrus_operator import WalrusOperatorTests
