fd309ca057ae62e5b6f13b3fb0f57a0a4a04e35c
Ariel Lin <arielin@fb.com>
2022-04-18 17:04:42 -0700

    add cpu and io mixed workload microbenchmark to async_tree script
    Summary: Add a new microbenchmark scenario, "cpu_io_mixed", to async_tree benchmark script, representing a recursive async tree workload with a mix of cpu-bound workload and IO-bound workload (simulated memoized IO calls) at all leaf node calls.
    
    Reviewed By: eduardo-elizondo
    
    Differential Revision: D35668465
    
    fbshipit-source-id: 53881a0eb02300e4c8923d7344197954dc0e5183

--- Tools/benchmarks/async_tree.py
+++ Tools/benchmarks/async_tree.py
@@ -7,6 +7,8 @@ following microbenchmark scenarios:
 2) "suspense_all": Suspension (simulating IO) at all leaf nodes in the async tree.
 3) "memoization": Simulated IO calls at all leaf nodes, but with memoization. Only
                   un-memoized IO calls will result in suspensions.
+4) "cpu_io_mixed": A mix of CPU-bound workload and IO-bound workload (with
+                   memoization) at the leaf nodes.
 
 Use the commandline flag or pass microbenchmark scenario name to run_microbenchmark()
 to determine which microbenchmark scenario to run.
@@ -14,6 +16,7 @@ to determine which microbenchmark scenario to run.
 
 
 import asyncio
+import math
 import random
 import time
 from argparse import ArgumentParser
@@ -23,6 +26,8 @@ NUM_RECURSE_LEVELS = 6
 NUM_RECURSE_BRANCHES = 6
 IO_SLEEP_TIME = 0.05
 DEFAULT_MEMOIZABLE_PERCENTAGE = 90
+DEFAULT_CPU_PROBABILITY = 0.5
+FACTORIAL_N = 500
 
 
 def parse_args():
@@ -36,7 +41,7 @@ to print the results.
     parser.add_argument(
         "-s",
         "--scenario",
-        choices=["no_suspension", "suspense_all", "memoization"],
+        choices=["no_suspension", "suspense_all", "memoization", "cpu_io_mixed"],
         default="no_suspension",
         help="""\
 Determines which microbenchmark scenario to run. Defaults to no_suspension. Options:
@@ -44,6 +49,8 @@ Determines which microbenchmark scenario to run. Defaults to no_suspension. Opti
 2) "suspense_all": Suspension (simulating IO) at all leaf nodes in the async tree.
 3) "memoization": Simulated IO calls at all leaf nodes, but with memoization. Only
                   un-memoized IO calls will result in suspensions.
+4) "cpu_io_mixed": A mix of CPU-bound workload and IO-bound workload (with
+                   memoization) at the leaf nodes.
 """,
     )
     parser.add_argument(
@@ -57,6 +64,17 @@ example, at the default 90 percent, data 1-90 will be memoized and data 91-100 w
 """,
     )
     parser.add_argument(
+        "-c",
+        "--cpu-probability",
+        type=float,
+        default=DEFAULT_CPU_PROBABILITY,
+        help="""\
+Sets the probability (0-1) that a leaf node will execute a cpu-bound workload instead
+of an io-bound workload. Defaults to 0.5. Only applies to the "cpu_io_mixed"
+microbenchmark scenario.
+""",
+    )
+    parser.add_argument(
         "-p",
         "--print",
         action="store_true",
@@ -67,10 +85,15 @@ example, at the default 90 percent, data 1-90 will be memoized and data 91-100 w
 
 
 class AsyncTree:
-    def __init__(self, memoizable_percentage=DEFAULT_MEMOIZABLE_PERCENTAGE):
+    def __init__(
+        self,
+        memoizable_percentage=DEFAULT_MEMOIZABLE_PERCENTAGE,
+        cpu_probability=DEFAULT_CPU_PROBABILITY,
+    ):
         self.suspense_count = 0
         self.task_count = 0
         self.memoizable_percentage = memoizable_percentage
+        self.cpu_probability = cpu_probability
         self.cache = {}
         # set to deterministic random, so that the results are reproducible
         random.seed(0)
@@ -112,11 +135,19 @@ class AsyncTree:
         await self.mock_io_call()
         return data
 
+    async def cpu_io_mixed_suspense_func(self):
+        if random.random() < self.cpu_probability:
+            # mock cpu-bound call
+            return math.factorial(FACTORIAL_N)
+        else:
+            return await self.memoization_suspense_func()
+
     def run_microbenchmark(self, scenario="no_suspension"):
         suspense_funcs = {
             "no_suspension": None,
             "suspense_all": self.suspense_all_suspense_func,
             "memoization": self.memoization_suspense_func,
+            "cpu_io_mixed": self.cpu_io_mixed_suspense_func,
         }
         suspense_func = suspense_funcs[scenario]
 
@@ -128,7 +159,7 @@ class AsyncTree:
 if __name__ == "__main__":
     args = parse_args()
     scenario = args.scenario
-    async_tree = AsyncTree(args.memoizable_percentage)
+    async_tree = AsyncTree(args.memoizable_percentage, args.cpu_probability)
 
     start_time = time.perf_counter()
     async_tree.run_microbenchmark(scenario)
