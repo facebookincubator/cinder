58a5a29707d213ca7957dce9d1c5d12c7d6f6cb2
Brett Simmers <bsimmers@fb.com>
2022-04-25 16:39:44 -0700

    Add Python tests that use profile data
    Summary:
    We already have `RuntimeTests/hir_tests/profile_data_test.txt` for
    unit-ish tests that use profile data, but we should also have some full
    end-to-end tests that include running the compiled code. This adds a skeleton
    of such a test and adds corresponding make targets to run the test in different
    JIT modes.
    
    Reviewed By: tekknolagi
    
    Differential Revision: D35764679
    
    fbshipit-source-id: e2fef993d1964a501f2c6d1b4ae59c5f158930c3

---
+++ Lib/test/test_cinderjit_profile.py
@@ -0,0 +1,16 @@
+# Copyright (c) Facebook, Inc. and its affiliates. (http://www.facebook.com)
+
+# NOTE: This test is run normally as part of the regular test suite, and it is
+# also run again separately, using profile data recorded for consumption by the
+# JIT. That means all of the tests in here are targeted at various JIT features
+# that only activate in the presence of profile data.
+
+import unittest
+
+
+class BinaryOpTests(unittest.TestCase):
+    def test_long_power(self):
+        def do_pow(a, b):
+            return a **b
+        self.assertEqual(do_pow(2, 8), 256)
+
