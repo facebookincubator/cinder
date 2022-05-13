39ec34876b1076242d3561bb5bbf5835de655420
Carl Meyer <carljm@fb.com>
2022-05-13 15:55:34 -0700

    pyre fixes
    Summary: A few minor changes to make Pyre happy.
    
    Reviewed By: sinancepel
    
    Differential Revision: D36381650
    
    fbshipit-source-id: 2225cc92438c85a750a1effd8748b1454b3e3655

--- Lib/compiler/pycodegen.py
+++ Lib/compiler/pycodegen.py
@@ -489,14 +489,16 @@ class CodeGenerator(ASTVisitor):
     ) -> bool:
         (ann_args, annotation_count) = self.annotate_args(node.args)
         # Cannot annotate return type for lambda
-        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)) and node.returns:
-            if self.module_gen.future_flags & consts.CO_FUTURE_ANNOTATIONS:
-                ann_args.append("return")
-                ann_args.append(to_expr(node.returns))
-            else:
-                self.emit("LOAD_CONST", "return")
-                self.visit(node.returns)
-                annotation_count += 2
+        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
+            returns = node.returns
+            if returns:
+                if self.module_gen.future_flags & consts.CO_FUTURE_ANNOTATIONS:
+                    ann_args.append("return")
+                    ann_args.append(to_expr(returns))
+                else:
+                    self.emit("LOAD_CONST", "return")
+                    self.visit(returns)
+                    annotation_count += 2
 
         if ann_args:
             self.emit("LOAD_CONST", tuple(ann_args))
@@ -550,15 +552,16 @@ class CodeGenerator(ASTVisitor):
             annotation_count += self.annotate_arg(arg, ann_args)
         return ann_args, annotation_count
 
-    def annotate_arg(self, arg: ast.arg, ann_args: List[str]) -> bool:
-        if arg.annotation:
+    def annotate_arg(self, arg: ast.arg, ann_args: List[str]) -> int:
+        ann = arg.annotation
+        if ann:
             if self.module_gen.future_flags & consts.CO_FUTURE_ANNOTATIONS:
                 ann_args.append(self.mangle(arg.arg))
-                ann_args.append(to_expr(arg.annotation))
+                ann_args.append(to_expr(ann))
                 return 2
             else:
                 self.emit("LOAD_CONST", self.mangle(arg.arg))
-                self.visit(arg.annotation)
+                self.visit(ann)
                 return 2
         return 0
 
@@ -2203,6 +2206,7 @@ class CodeGenerator(ASTVisitor):
             self.emit("POP_BLOCK")
             if preserve_tos:
                 self.setups.push(Entry(POP_VALUE, None, None, None))
+            assert callable(e.unwinding_datum)
             e.unwinding_datum()
         elif e.kind in (WITH, ASYNC_WITH):
             self.emit("POP_BLOCK")
