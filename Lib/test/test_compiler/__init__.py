f16fd60812adb3cd3b55ad58a0f9ddefb896db64
Carl Meyer <carljm@fb.com>
2022-06-30 18:13:09 -0700

    Update error message in compiler
    Summary:
    Update a SyntaxError message to match its wording in Python 3.10.
    
    This allows enabling some more compiler tests.
    
    Reviewed By: itamaro
    
    Differential Revision: D37570694
    
    fbshipit-source-id: fbacfb8280599954bd7b0e3baff23a52b4e5f1d3

--- Lib/test/test_compiler/__init__.py
+++ Lib/test/test_compiler/__init__.py
@@ -4,7 +4,7 @@ from .test_api import ApiTests
 
 # from .test_code_sbs import CodeTests
 # from .test_corpus import SbsCorpusCompileTests
-from .test_errors import ErrorTests  # , ErrorTestsBuiltin
+from .test_errors import ErrorTests, ErrorTestsBuiltin
 from .test_flags import FlagTests
 from .test_graph import GraphTests
 
