d93290c23be0135f10670c55075e086b89673753
Tzu-Chuan Lin <tzuchuan.l@gmail.com>
2022-07-05 09:06:54 -0700

    Update reasons

--- Lib/test/test_lazy_imports.py
+++ Lib/test/test_lazy_imports.py
@@ -5,11 +5,11 @@ class TestLazyImports(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestLazyImports, self).__init__(*args, **kwargs)
 
-    @unittest.skipIfLazyImportsIsDisabled("Skip this test when Lazy Import is disabled.")
+    @unittest.skipIfLazyImportsIsDisabled("Test relevant only when running with lazy imports enabled.")
     def test_lazy_imports_is_enabled(self):
         self.assertTrue(importlib.is_lazy_imports_enabled())
 
-    @unittest.skipIfLazyImportsIsEnabled("Skip this test when Lazy Import is enabled.")
+    @unittest.skipIfLazyImportsIsEnabled("Test relevant only when running with lazy imports disabled.")
     def test_lazy_imports_is_disabled(self):
         self.assertFalse(importlib.is_lazy_imports_enabled())
 
