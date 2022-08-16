393997e3781e2aafeaf7c7f8381d03171f65d278
Brittany Reynoso <breynoso@fb.com>
2022-08-15 17:51:56 -0700

    Expose module tests
    Summary: Expose additional test suite
    
    Reviewed By: sinancepel
    
    Differential Revision: D38716102
    
    fbshipit-source-id: 741cc990c7e8f3d2d7ad95d905baf005c081d387

--- Lib/test/test_compiler/test_static/__init__.py
+++ Lib/test/test_compiler/test_static/__init__.py
@@ -28,7 +28,10 @@ from .invoke import InvokeTests
 from .lint import LintTests
 
 # from .literals import LiteralsTests
-# from .module import ModuleTests
+
+
+from .module import ModuleTests
+
 # from .non_static_inheritance import NonStaticInheritanceTests
 # from .obj_creation import StaticObjCreationTests
 # from .overrides import OverridesTests
