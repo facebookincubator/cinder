d892ddaed36e1e1be81d8789e7c370e877c81f30
Aniket Panse <aniketpanse@fb.com>
2022-08-18 09:57:07 -0700

    Enable the "strict_compile" function tests
    Summary: They're currently disabled. Enabling them causes refleak tests to fail, so we only enable them in non-debug builds (see comment for details).
    
    Reviewed By: sinancepel
    
    Differential Revision: D38807492
    
    fbshipit-source-id: 65569447e825e8c3aadd0213a0b16ba6a4b7f28b

--- Lib/test/test_compiler/test_strict/__init__.py
+++ Lib/test/test_compiler/test_strict/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from .test_compiler import CompilerTests, GetModuleKindTest
 from .test_definite_assignment import DefiniteAssignmentTests
 from .test_loader import StrictLoaderInstallTest, StrictLoaderTest
@@ -14,3 +16,10 @@ from .test_strict_codegen import (
     StrictCheckedCompilationTests,
     StrictCompilationTests,
 )
+
+if not hasattr(sys, "gettotalrefcount"):
+    # TODO(T129388950) These tests are only enabled in opt builds for now.
+    # We need to fix the refleak in the strict compilation tests.
+    # The "leak" here is just Static Python types (e.g ResolvedTypeRef),
+    # we need to clean them up after compilation is done.
+    from .test_strict_compile import StrictCompileTest
