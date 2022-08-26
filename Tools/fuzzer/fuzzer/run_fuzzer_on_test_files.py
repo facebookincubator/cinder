d2365f9a3ef6e4cc3918cf0cdff10ece3a382c97
Max Bernstein <emacs@fb.com>
2022-08-26 10:40:39 -0700

    Fix bugs in run_fuzzer_on_test_files
    Summary:
    Original Diff: D38430776 (https://github.com/facebookincubator/cinder/commit/3c66ea900ccb82cc5481725f21fee0e568908236) by ananyajhaveri, reviewed by sinancepel. Applied cleanly.
    
    Fixed bug where file directories were incorrect in `run_fuzzer_on_test_files`, causing it to run the fuzzer on the same file repeatedly. Modified run directory in for `make run_fuzzer_on_test_files` to be in the cinder folder as opposed to the fuzzer folder. Additionally, stdout and stderr output was not appearing on Skycastle since they are stored in `fuzzer_output.txt`, so added a print statement to print file contents to stdout.
    
    Reviewed By: sinancepel
    
    Differential Revision: D39039451
    
    fbshipit-source-id: 32323f0ee01a504fbfd8ad2179c1315dd0557d20

--- Tools/fuzzer/fuzzer/run_fuzzer_on_test_files.py
+++ Tools/fuzzer/fuzzer/run_fuzzer_on_test_files.py
@@ -1,17 +1,18 @@
 from executor import run_fuzzer_on_test_file
 
+# File directories are relative to cinder directory
 # Test file list is subject to change
 TEST_FILE_LIST = [
-    "/Lib/ctypes/test/test_anon.py",
-    "/Lib/ctypes/test/test_array_in_pointer.py",
-    "/Lib/ctypes/test/test_bitfields.py",
-    "/Lib/ctypes/test/test_callbacks.py",
-    "/Lib/test/test_fileio.py",
-    "/Lib/test/test_itertools.py",
-    "/Lib/test/test_memoryview.py",
-    "/Tools/fuzzer/tests/verifier_test.py",
-    "/Tools/fuzzer/tests/fuzzer_test.py",
-    "/Tools/fuzzer/tests/executor_test.py",
+    "Lib/ctypes/test/test_anon.py",
+    "Lib/ctypes/test/test_array_in_pointer.py",
+    "Lib/ctypes/test/test_bitfields.py",
+    "Lib/ctypes/test/test_callbacks.py",
+    "Lib/test/test_fileio.py",
+    "Lib/test/test_itertools.py",
+    "Lib/test/test_memoryview.py",
+    "Tools/fuzzer/tests/verifier_test.py",
+    "Tools/fuzzer/tests/fuzzer_test.py",
+    "Tools/fuzzer/tests/executor_test.py",
 ]
 
 # Number of subprocesses are also subject to change
@@ -19,13 +20,17 @@ SUBPROCESSES = 4
 
 
 def run_fuzzer_on_test_files():
-    f = open("fuzzer_output.txt", "w+")
-    for i in TEST_FILE_LIST:
-        run_fuzzer_on_test_file(
-            __file__.replace("/Tools/fuzzer/fuzzer/run_fuzzer_on_test_files.py", i),
-            SUBPROCESSES,
-            f,
-        )
+    with open("fuzzer_output.txt", "w+") as outfile:
+        for i in TEST_FILE_LIST:
+            run_fuzzer_on_test_file(
+                i,
+                SUBPROCESSES,
+                outfile,
+            )
+    # stdout and stderr stored in file
+    # need to print so its available on skycastle
+    with open("fuzzer_output.txt", "r") as outfile:
+        print(outfile.read())
 
 
 if __name__ == "__main__":
