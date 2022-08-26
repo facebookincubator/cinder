91c81355c0a23fbd4ec56cdd8e6d3b5a24c874d2
Max Bernstein <emacs@fb.com>
2022-08-26 10:40:29 -0700

    Add ability for fuzzer to run on test files
    Summary:
    Original Diff: D38042539 (https://github.com/facebookincubator/cinder/commit/2e0849d411be405fdaf405becd88eab6711dec1e) by ananyajhaveri, reviewed by sinancepel. Applied cleanly.
    
    Added functions in `executor.py` to allow fuzzer to parse and fuzz functions found in test files.
    
    Created `executor_test.py` to test whether `executor.py` can extract functions from a python file correctly. Added `executor_test.py` to the makefile so it can be tested using `test_fuzzer`, `test_fuzzer_jit`, or `test_fuzzer_jit_inliner` both locally and in the CI.
    
    Can try to fuzz a file by running `./python [path to cinder folder]/Tools/fuzzer/fuzzer/executor.py --file "[file location]" --subprocesses [number of subprocesses]`
    
    Reviewed By: sinancepel
    
    Differential Revision: D39039669
    
    fbshipit-source-id: 9a8717023f33f38e6eb728104eaad89101805adf

--- Tools/fuzzer/fuzzer/executor.py
+++ Tools/fuzzer/fuzzer/executor.py
@@ -1,6 +1,13 @@
 import argparse
+import ast
+import io
+import re
 import subprocess
 import sys
+import textwrap
+from typing import List, Tuple
+
+import unparse
 
 
 def run_fuzzer(code_str: str, subprocesses: int) -> None:
@@ -22,10 +29,48 @@ def run_fuzzer(code_str: str, subprocesses: int) -> None:
         i.wait()
 
 
+# calls run_fuzzer on every function in a test file
+def run_fuzzer_on_test_file(file_location: str, subprocesses: int) -> None:
+    funcs = extract_functions_from_file(file_location)
+    subprocess_arr = []
+    for i in funcs:
+        run_fuzzer(i, subprocesses)
+
+
+# extracts function strings from a file
+# by parsing a file into an AST
+# and unparsing embedded function objects
+def extract_functions_from_file(file_location: str) -> List[str]:
+    funcs = []
+    with open(file_location, "r") as code_file:
+        text = code_file.read()
+        node = ast.parse(text)
+        s = [i for i in node.body]
+        # extract all embedded function objects by recursing with a stack
+        while s:
+            curr = s.pop()
+            str_io = io.StringIO()
+            # unparse function objects and append them to funcs
+            if isinstance(curr, ast.FunctionDef):
+                unparsed_func = unparse.Unparser(curr, str_io)
+                funcs.append(str_io.getvalue())
+            if hasattr(curr, "body"):
+                s += [
+                    i
+                    for i in curr.body
+                    if isinstance(i, ast.FunctionDef) or isinstance(i, ast.ClassDef)
+                ]
+    return funcs
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--codestr", help="code string to be passed into the fuzzer")
+
     parser.add_argument("--subprocesses", help="number of subprocesses")
+    parser.add_argument("--file", help="location of file to run fuzzer on")
     args = parser.parse_args()
     if args.codestr and args.subprocesses:
         run_fuzzer(args.codestr, int(args.subprocesses))
+    elif args.file and args.subprocesses:
+        run_fuzzer_on_test_file(args.file, int(args.subprocesses))
---
+++ Tools/fuzzer/tests/executor_test.py
@@ -0,0 +1,55 @@
+import os
+import re
+import sys
+import unittest
+from compiler import compile
+from inspect import cleandoc
+from tempfile import NamedTemporaryFile
+
+sys.path.append(os.path.join(sys.path[0], "..", "fuzzer"))
+from executor import extract_functions_from_file
+
+
+class ExecutorExtractFunctionFromFileTest(unittest.TestCase):
+    test_string = cleandoc(
+        """
+        def foo():
+            x = 7
+            return x
+
+        class Bar:
+            def xyz():
+                a = 5
+                a += 2
+                return a
+        """
+    )
+
+    def test_correct_number_of_functions_extracted(self):
+        with NamedTemporaryFile(mode=("w+")) as temp_file:
+            temp_file.write(self.test_string)
+            temp_file.flush()
+            funcs = extract_functions_from_file(temp_file.name)
+            self.assertEqual(len(funcs), 2)
+
+    def test_extracted_strings_are_all_compilable_functions(self):
+        with NamedTemporaryFile(mode=("w+")) as temp_file:
+            temp_file.write(self.test_string)
+            temp_file.flush()
+            funcs = extract_functions_from_file(temp_file.name)
+            for i in funcs:
+                self.assertEqual(i.strip()[:3], "def")
+                code = compile(i, "", "exec")
+
+    def test_function_indentation_level_is_consistent(self):
+        with NamedTemporaryFile(mode=("w+")) as temp_file:
+            temp_file.write(self.test_string)
+            temp_file.flush()
+            funcs = extract_functions_from_file(temp_file.name)
+            for i in funcs:
+                indentation = len(re.findall("^ *", i)[0])
+                self.assertEqual(indentation, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
