8c08bedf8d800008b6990b12e272928696c1cdd5
Itamar Ostricher <itamaro@fb.com>
2022-10-31 14:52:32 -0700

    Enable more Cinder OSS tests
    Reviewed By: tekknolagi
    
    Differential Revision: D40839387
    
    fbshipit-source-id: 36da1a3ef4eb1de67ffe7740c5a0bc368e68b859

--- oss-cinder-test.sh
+++ oss-cinder-test.sh
@@ -6,7 +6,7 @@ set -xe
 env
 
 ./python -VV
-# make -j 4 VERBOSE=1 testcinder_jit
+make -j 4 VERBOSE=1 testcinder_jit
 # make -j 4 VERBOSE=1 testruntime
 make -j 4 VERBOSE=1 testcinder
-# make -j 4 VERBOSE=1 test_strict_module
+make -j 4 VERBOSE=1 test_strict_module
