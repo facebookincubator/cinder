0f77fd3efea20d28feacaadd289117b65b168a5c
Max Bernstein <emacs@fb.com>
2022-11-07 10:49:27 -0800

    Update magic number after compileJumpIf fix
    Summary:
    D41051510 (https://github.com/facebookincubator/cinder/commit/6bc198c3f5782aa14f78f609807527fdd76a0bb6) changed the bytecode but did not bump the magic
    number. This is needed to mark bytecode as stale and get recompiled with
    the new compiler version.
    
    Reviewed By: brittanyrey
    
    Differential Revision: D41087742
    
    fbshipit-source-id: 1fe9eeab6c5480254d70594ac80ada64b3db82a0

--- Lib/compiler/strict/common.py
+++ Lib/compiler/strict/common.py
@@ -51,7 +51,7 @@ from .track_import_call import tracker
 # Increment this whenever we change the output of the strict modules
 # interpreter. It must stay below 32768 (15 bits), because we use the high bit
 # to encode strictness of the module.
-MAGIC_NUMBER = 42
+MAGIC_NUMBER = 43
 
 
 DEFAULT_STUB_PATH = os.path.dirname(__file__) + "/stubs"
