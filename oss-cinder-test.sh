923f27f198f9de753f365e192cb93317fb568aed
Itamar Ostricher <itamaro@meta.com>
2023-01-22 19:23:14 -0800

    Try enabling testruntime in OSS CI

--- oss-cinder-test.sh
+++ oss-cinder-test.sh
@@ -7,6 +7,6 @@ env
 
 ./python -VV
 make -j 4 VERBOSE=1 testcinder_jit
-# make -j 4 VERBOSE=1 testruntime
+make -j 4 VERBOSE=1 testruntime
 make -j 4 VERBOSE=1 testcinder
 make -j 4 VERBOSE=1 test_strict_module
