e01210583acaecdb6cc45fa8c1826ff3a37ca4b8
Jacob Bower <jbower@fb.com>
2023-03-13 15:47:31 -0700

    Add test for regressions in CinderVM <-> core Python coupling
    Summary:
    Now we have the ability to build CinderVM as a `.so` we can build a `libpython.so` which would be used by the CinderVM `.so` and look at the `libpython.so` exported symbols. If these symbols change, it means we altered the API needed by the CinderVM `.so` (assuming the base CPython isn't changing). If we see new symbols we can detect this and suggest the user ensures they are exported with `CiAPI_` macros.
    
    Generally the idea is to raise-the-bar a little for adding new couplings.
    
    Reviewed By: itamaro
    
    Differential Revision: D42908977
    
    fbshipit-source-id: ec49d98eeee82f630a9d08d24ebe67ac0db11a5e

---
+++ Cinder/module/make-current-symbols.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Generates a list of symbols exported from "core" (non-CinderVM) Python.
+#
+# To be run after a Cinder VM .so build which additionally has --enable-shared
+# configured. E.g to match Sandcatle:
+#
+#   OPT=-O2 python3 Tools/scripts/facebook/configure_with_fb_toolchain.py --shared --debug -- --enable-cinder-vm-so
+#   make -j
+#
+# Note the "OPT=-O2".
+
+set -xe
+
+ROOT=$(git rev-parse --show-toplevel)
+
+# The greps make sure to only include exported text and data symbols, and strip
+# out empty lines respectively.
+nm "$ROOT"/libpython3.10d.so | grep -E '^[0-9a-f]* T|D' | cut -f 3 -d " " | grep -vE '^$' | sort -u
