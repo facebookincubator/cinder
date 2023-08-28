93bbe8e7bf84e88b9bc80b87fe4d0dfb5fa7389f
Johnston Jiaa <jjiaa@meta.com>
2023-08-28 15:19:26 -0700

    Clean up _cinderx.cpp indent style and init
    Summary:
    - Removes unused `hello` method.
    - Updates the module docstring.
    - Converts to using two-space indents.
    - We need to use `PyModuleDef_Init`
      - We'll be adding an exec slot.
      - `PyModuleDef_Create` isn't compatible with exec slots.
    
    Reviewed By: carljm
    
    Differential Revision: D48750459
    
    fbshipit-source-id: 4f62a65def12f0b08b9ad0956ef3678fb206ab94

--- CinderX/_cinderx.cpp
+++ CinderX/_cinderx.cpp
@@ -1,35 +1,20 @@
 #include <Python.h>
 
-static PyObject *hello(PyObject *Py_UNUSED(self), PyObject *Py_UNUSED(arg)) {
-  Py_RETURN_NONE;
-}
-
 static PyMethodDef _cinderx_methods[] = {
-    {"hello", reinterpret_cast<PyCFunction>(hello), METH_NOARGS, "Say hello"},
-    {nullptr, nullptr, 0, nullptr}};
+  {nullptr, nullptr, 0, nullptr}};
 
 static struct PyModuleDef _cinderxmodule = {
-    PyModuleDef_HEAD_INIT,
-    "_cinderx",
-    "A sample Cinder extension module",
-    /*m_size=*/0,
-    _cinderx_methods,
-    /*m_slots=*/nullptr,
-    /*m_traverse=*/nullptr,
-    /*m_clear=*/nullptr,
-    /*m_free=*/nullptr,
+  PyModuleDef_HEAD_INIT,
+  "_cinderx",
+  "The internal CinderX extension module",
+  /*m_size=*/0,
+  _cinderx_methods,
+  /*m_slots=*/nullptr,
+  /*m_traverse=*/nullptr,
+  /*m_clear=*/nullptr,
+  /*m_free=*/nullptr,
 };
 
 PyMODINIT_FUNC PyInit__cinderx(void) {
-  PyObject *module = PyState_FindModule(&_cinderxmodule);
-  if (module != nullptr) {
-    Py_INCREF(module);
-    return module;
-  }
-  module = PyModule_Create(&_cinderxmodule);
-  if (module == nullptr) {
-    return nullptr;
-  }
-  PyState_AddModule(module, &_cinderxmodule);
-  return module;
+  return PyModuleDef_Init(&_cinderxmodule);
 }
