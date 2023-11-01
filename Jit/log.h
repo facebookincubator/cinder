69fffb8652ac8658ccb62466f5798d4c602e1796
Alex Malyshev <alexanderm@meta.com>
2023-11-01 08:34:58 -0700

    Delete the printf forms of JIT log macros
    Summary: No longer needed now that they're all codemodded away.
    
    Reviewed By: mpage
    
    Differential Revision: D50743413
    
    fbshipit-source-id: 125e2fa8cc2fb7ab6539a5dca72288b17186ebef

--- Jit/log.h
+++ Jit/log.h
@@ -49,18 +49,6 @@ extern FILE* g_log_file;
 // "<failed to get UTF8 from Python string>"
 std::string repr(BorrowedRef<> obj);
 
-// fmt doesn't support compile-time checking of printf-style format strings, so
-// this wrapper is used to contain any exceptions rather than aborting during a
-// JIT_LOGX() or unwinding the stack after a failed JIT_CHECKX().
-template <typename... Args>
-void protected_fprintf(std::FILE* file, std::string_view fmt, Args&&... args) {
-  try {
-    fmt::fprintf(file, fmt, std::forward<Args>(args)...);
-  } catch (const fmt::format_error& fe) {
-    fmt::fprintf(file, "Bad format string '%s': '%s'", fmt, fe.what());
-  }
-}
-
 #define JIT_LOG(...)                                                    \
   {                                                                      \
     ::jit::ThreadedCompileSerialize guard;                               \
@@ -70,27 +58,12 @@ void protected_fprintf(std::FILE* file, std::string_view fmt, Args&&... args) {
     std::fflush(::jit::g_log_file);                                      \
   }
 
-#define JIT_LOGX(...)                                                     \
-  {                                                                      \
-    ::jit::ThreadedCompileSerialize guard;                               \
-    fmt::print(::jit::g_log_file, "JIT: {}:{} -- ", __FILE__, __LINE__); \
-    ::jit::protected_fprintf(::jit::g_log_file, __VA_ARGS__);            \
-    fmt::print(::jit::g_log_file, "\n");                                 \
-    std::fflush(::jit::g_log_file);                                      \
-  }
-
 #define JIT_LOGIF(PRED, ...) \
   if (PRED) {                \
     JIT_LOG(__VA_ARGS__);    \
   }
 
-#define JIT_LOGIFX(PRED, ...) \
-  if (PRED) {                 \
-    JIT_LOGX(__VA_ARGS__);    \
-  }
-
 #define JIT_DLOG(...) JIT_LOGIF(::jit::g_debug_verbose, __VA_ARGS__)
-#define JIT_DLOGX(...) JIT_LOGIFX(::jit::g_debug_verbose, __VA_ARGS__)
 
 #define JIT_CHECK(COND, ...)                      \
   {                                               \
@@ -105,29 +78,10 @@ void protected_fprintf(std::FILE* file, std::string_view fmt, Args&&... args) {
     }                                             \
   }
 
-#define JIT_CHECKX(COND, ...)                     \
-  {                                               \
-    if (!(COND)) {                                \
-      fmt::print(                                 \
-          stderr,                                 \
-          "JIT: {}:{} -- Assertion failed: {}\n", \
-          __FILE__,                               \
-          __LINE__,                               \
-          #COND);                                 \
-      JIT_ABORT_IMPLX(__VA_ARGS__);               \
-    }                                             \
-  }
-
 #define JIT_ABORT(...)                                               \
   {                                                                  \
     fmt::print(stderr, "JIT: {}:{} -- Abort\n", __FILE__, __LINE__); \
     JIT_ABORT_IMPL(__VA_ARGS__);                                     \
-  }
-
-#define JIT_ABORTX(...)                                              \
-  {                                                                  \
-    fmt::print(stderr, "JIT: {}:{} -- Abort\n", __FILE__, __LINE__); \
-    JIT_ABORT_IMPLX(__VA_ARGS__);                                    \
   }
 
 #define JIT_ABORT_IMPL(...)                              \
@@ -145,32 +99,12 @@ void protected_fprintf(std::FILE* file, std::string_view fmt, Args&&... args) {
     std::abort();                                        \
   }
 
-#define JIT_ABORT_IMPLX(...)                              \
-  {                                                      \
-    ::jit::protected_fprintf(stderr, __VA_ARGS__);       \
-    fmt::print(stderr, "\n");                            \
-    std::fflush(stderr);                                 \
-    PyThreadState* tstate = _PyThreadState_GET();        \
-    if (tstate != NULL && tstate->curexc_type != NULL) { \
-      PyErr_Display(                                     \
-          tstate->curexc_type,                           \
-          tstate->curexc_value,                          \
-          tstate->curexc_traceback);                     \
-    }                                                    \
-    std::abort();                                        \
-  }
-
 #ifdef Py_DEBUG
 #define JIT_DCHECK(COND, ...) JIT_CHECK(COND, __VA_ARGS__)
-#define JIT_DCHECKX(COND, ...) JIT_CHECKX(COND, __VA_ARGS__)
 #else
 #define JIT_DCHECK(COND, ...)       \
   if (0) {                          \
     JIT_CHECK((COND), __VA_ARGS__); \
-  }
-#define JIT_DCHECKX(COND, ...)       \
-  if (0) {                           \
-    JIT_CHECKX((COND), __VA_ARGS__); \
   }
 #endif
 
