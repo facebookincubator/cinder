63aadb9c35be8b043e37bd3dcd60d0830e1867cb
Jacob Bower <jbower@meta.com>
2023-12-27 11:52:39 -0800

    Revert "Switch CinderX over to being a True Module(TM)"
    Summary:
    This reverts commit 6a6fb54e46fa7acbf1dc2a9d9859cb7801dcb5c8.
    
    That commit broke the OSS build in a very weird way, and I suspect it's also breaking some of our CI tests internally although I'm not 100% sure it's the cause of that breakage yet.
    
    Reviewed By: pilleye
    
    Differential Revision: D52433973
    
    fbshipit-source-id: 7ef9cb29b4ae052243e1e8b3bf68d08669efca5c

--- CinderX/build.sh
+++ CinderX/build.sh
@@ -1,65 +1,26 @@
 #!/bin/bash
 
+ROOT=$(
+  git rev-parse --is-inside-work-tree >/dev/null 2>&1 &&
+  git rev-parse --show-toplevel ||
+  hg root)
+MODULE_DIR=$(readlink -f "$(dirname "$0")")
+PYTHON_FOR_CINDERX_BUILD_DIR="$ROOT/build_cinderx_venv"
+VENV_DIR="$PYTHON_FOR_CINDERX_BUILD_DIR/venv"
+
+if ! [ -f "$VENV_DIR"/bin/activate ]; then
+  >&2 echo "Run $MODULE_DIR/bootstrap.sh with a clean checkout."
+  >&2 echo "This only needs to be done once."
+  exit 1
+fi
+
 set -xe
 
-MODULE_SRC_DIR=$(readlink -f "$(dirname "$0")")
+. "$VENV_DIR"/bin/activate
 
-BUILD_ROOT=""
-PYTHON_BIN=""
-OUTPUT_DIR=""
-
-# Parse command line arguments
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --build-root)
-      BUILD_ROOT="$2"
-      shift # past value
-      ;;
-    --python-bin)
-      PYTHON_BIN="$2"
-      shift # past value
-      ;;
-    --output-dir)
-      OUTPUT_DIR="$2"
-      shift # past value
-      ;;
-    *)
-      echo "Unknown option: $key"
-      exit 1
-      ;;
-  esac
-  shift # past argument
-done
-
-if [ -z "$BUILD_ROOT" ] ; then
-  BUILD_ROOT=$(
-    git rev-parse --is-inside-work-tree >/dev/null 2>&1 &&
-    git rev-parse --show-toplevel ||
-    hg root)
-fi
-
-if [ -z "$PYTHON_BIN" ] ; then
-  PYTHON_BIN="$BUILD_ROOT/python"
-fi
-
-# Make venv for setuptools
-VENV_DIR="$BUILD_ROOT/_cinderx_venv/"
-if ! [ -f "$VENV_DIR/bin/activate" ] ; then
-  rm -rf "$VENV_DIR"
-  "$PYTHON_BIN" -mvenv --without-pip "$VENV_DIR"
-  . "$VENV_DIR"/bin/activate
-  # Uses Python from our venv
-  python3 "$MODULE_SRC_DIR"/pip.pyz install "$MODULE_SRC_DIR"/setuptools-65.6.0-py3-none-any.whl
-else
-  . "$VENV_DIR"/bin/activate
-fi
-
-# Build the cinderx module
-MODULE_BUILD_DIR="$BUILD_ROOT/_cinderx_build/"
-cd "$MODULE_SRC_DIR"
+# Build the cinderx "module" .so
+cd "$MODULE_DIR"
+rm -rf "$MODULE_DIR"/build
 # Uses Python from our venv
-python3 setup.py build -b "$MODULE_BUILD_DIR"
-
-if ! [ -z "$OUTPUT_DIR" ] ; then
-  ln -sf "$MODULE_BUILD_DIR"/lib.*/*so "$OUTPUT_DIR/"
-fi
+python3 setup.py build
+ln -s "$MODULE_DIR"/build/lib.*/*so "$MODULE_DIR"/build
