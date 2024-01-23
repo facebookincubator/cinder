876864c9e53c824bea5fa0b7b262889fe253d3b5
Carl Meyer <carljm@meta.com>
2024-01-22 19:08:19 -0800

    run pyfmt on cinderx/cinderx/*.py
    Summary:
    This diff just runs `pyfmt` on the `.py` files in `cinderx/cinderx/`, which have been accidentally exempt from linting for a while. The next diff re-enables linting on them.
    
    The only manual change is to remove a couple grouping comments in `cinderx/cinderx/__init__.py` which are no longer accurate once the imports are auto-sorted alphabetically.
    
    Reviewed By: swtaarrs
    
    Differential Revision: D52862474
    
    fbshipit-source-id: fb48d154ccfaecb130730d1aab4d3ee1990fe6ee

--- cinderx/cinderx/__init__.py
+++ cinderx/cinderx/__init__.py
@@ -10,35 +10,32 @@ try:
     sys.setdlopenflags(old_dlopen_flags | os.RTLD_GLOBAL)
     try:
         from _cinderx import (
-            # Methods
             _compile_perf_trampoline_pre_fork,
-            _get_entire_call_stack_as_qualnames_with_lineno_and_frame,
             _get_entire_call_stack_as_qualnames_with_lineno,
+            _get_entire_call_stack_as_qualnames_with_lineno_and_frame,
             _is_compile_perf_trampoline_pre_fork_enabled,
+            async_cached_classproperty,
+            async_cached_property,
+            cached_classproperty,
+            cached_property,
             clear_all_shadow_caches,
             clear_caches,
             clear_classloader_caches,
             clear_type_profiles,
             disable_parallel_gc,
             enable_parallel_gc,
-            get_and_clear_type_profiles_with_metadata,
             get_and_clear_type_profiles,
+            get_and_clear_type_profiles_with_metadata,
             get_parallel_gc_settings,
             init,
+            set_profile_interp,
             set_profile_interp_all,
             set_profile_interp_period,
-            set_profile_interp,
+            strict_module_patch,
             strict_module_patch_delete,
             strict_module_patch_enabled,
-            strict_module_patch,
-            watch_sys_modules,
-
-            # Other attributes
-            async_cached_classproperty,
-            async_cached_property,
-            cached_classproperty,
-            cached_property,
             StrictModule,
+            watch_sys_modules,
         )
     finally:
         sys.setdlopenflags(old_dlopen_flags)
--- cinderx/cinderx/strictmodule.py
+++ cinderx/cinderx/strictmodule.py
@@ -2,10 +2,10 @@ from _strictmodule import (
     NONSTRICT_MODULE_KIND,
     STATIC_MODULE_KIND,
     STRICT_MODULE_KIND,
+    StrictAnalysisResult,
+    StrictModuleLoader,
     STUB_KIND_MASK_ALLOWLIST,
     STUB_KIND_MASK_NONE,
     STUB_KIND_MASK_STRICT,
     STUB_KIND_MASK_TYPING,
-    StrictAnalysisResult,
-    StrictModuleLoader,
 )
