96342579444a5c4ce691dba1e1b9d8fb1c90996d
Jacob Bower <jbower@meta.com>
2024-01-24 12:55:24 -0800

    Re-enables the test to check CinderX can be built in fbcode
    Summary: The test can use new `enable_cinderx_fbcode` feature of `cinder_binary()` once the dependent diffs are landed.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D52826731
    
    fbshipit-source-id: 862b691ec2f96af691ea816ecd1b8ad5ca5d5974

--- cinderx/assert_fbcode__cinderx_so.py
+++ cinderx/assert_fbcode__cinderx_so.py
@@ -1,10 +1,6 @@
-import os
 import sys
 
 def main():
-    sys.setdlopenflags(sys.getdlopenflags() | os.RTLD_GLOBAL)
-    import _cinderx
-
     print(f"_cinderx: {sys.modules['_cinderx']}")
     if str(sys.modules["_cinderx"]) != "<module '_cinderx' (static-extension)>":
         raise AssertionError("_cinderx is not from fbcode")
