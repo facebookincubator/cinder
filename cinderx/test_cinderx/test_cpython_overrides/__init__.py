c79461f4d7adc7ae31551a1e648484172910686e
Aditya Pillai <pilleye@meta.com>
2024-01-31 07:42:55 -0800

    extract tests from test_gdb.py
    Summary: above
    
    Reviewed By: jbower-fb
    
    Differential Revision: D53019355
    
    fbshipit-source-id: 2cfff021bb8af0818747a1ab3393e892dc13ef93

--- cinderx/test_cinderx/test_cpython_overrides/__init__.py
+++ cinderx/test_cinderx/test_cpython_overrides/__init__.py
@@ -3,4 +3,5 @@ from .test_asyncgen import CinderX_AsyncGenAsyncioTest
 from .test_capi import CinderX_CAPITest
 from .test_dis import CinderX_DisTests
 from .test_fork1 import CinderX_ForkTest
+from .test_gdb import CinderX_DebuggerTests, CinderX_PrettyPrintTests
 from .test_repl import CinderX_TestInteractiveInterpreter
