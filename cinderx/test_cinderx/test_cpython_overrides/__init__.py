3079173d6c87be954d02ffdb1f7dbf884c87f64f
Aditya Pillai <pilleye@meta.com>
2024-02-06 14:22:21 -0800

    extract tests from test_dis.py
    Summary: Extracts tests added from D49690736.
    
    Reviewed By: jbower-fb
    
    Differential Revision: D53285953
    
    fbshipit-source-id: 1f5742f31d976cb022efa490b532784819869010

--- cinderx/test_cinderx/test_cpython_overrides/__init__.py
+++ cinderx/test_cinderx/test_cpython_overrides/__init__.py
@@ -1,7 +1,7 @@
 from .test__opcode import CinderX_OpcodeTests
 from .test_asyncgen import CinderX_AsyncGenAsyncioTest
 from .test_capi import CinderX_CAPITest
-from .test_dis import CinderX_DisTests
+from .test_dis import CinderX_DisTests, CinderX_DisWithFileTests
 from .test_fork1 import CinderX_ForkTest
 from .test_gdb import CinderX_DebuggerTests, CinderX_PrettyPrintTests
 from .test_generators import CinderX_GeneratorTest
