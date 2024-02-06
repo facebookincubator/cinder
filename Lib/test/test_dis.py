0631e31726247fcc6d2eef81afd954f7dc2aba71
Aditya Pillai <pilleye@meta.com>
2024-02-06 14:22:24 -0800

    extract tests from test_dis.py
    Summary: Reverts ports 3.8 -> 3.10 from D40656285, D39216342, D38953111
    
    Reviewed By: jbower-fb
    
    Differential Revision: D53287220
    
    fbshipit-source-id: 6703de973c7b09f9b7c036f3568ae53e9cb0ff7d

--- Lib/test/test_dis.py
+++ Lib/test/test_dis.py
@@ -2,7 +2,6 @@
 
 from test.support import captured_stdout
 from test.support.bytecode_helper import BytecodeTestCase
-from textwrap import dedent
 import unittest
 import sys
 import dis
@@ -149,35 +148,30 @@ def compile_and_get(code_str, funcname):
     return _tmp_globals[funcname]
 
 
-bug1333982_str = """
 def bug1333982(x=[]):
     assert 0, ([s for s in x] +
               1)
     pass
-"""
-bug1333982 = compile_and_get(bug1333982_str, "bug1333982")
 
 dis_bug1333982 = """\
 %3d           0 LOAD_ASSERTION_ERROR
-              2 BUILD_LIST               0
-              4 LOAD_FAST                0 (x)
-              6 GET_ITER
-        >>    8 FOR_ITER                 4 (to 18)
-             10 STORE_FAST               1 (s)
-             12 LOAD_FAST                1 (s)
-             14 LIST_APPEND              2
-             16 JUMP_ABSOLUTE            4 (to 8)
-        >>   18 DELETE_FAST              1 (s)
+              2 LOAD_CONST               2 (<code object <listcomp> at 0x..., file "%s", line %d>)
+              4 LOAD_CONST               3 ('bug1333982.<locals>.<listcomp>')
+              6 MAKE_FUNCTION            0
+              8 LOAD_FAST                0 (x)
+             10 GET_ITER
+             12 CALL_FUNCTION            1
 
-%3d          20 LOAD_CONST               2 (1)
+%3d          14 LOAD_CONST               4 (1)
 
-%3d          22 BINARY_ADD
-             24 CALL_FUNCTION            1
-             26 RAISE_VARARGS            1
+%3d          16 BINARY_ADD
+             18 CALL_FUNCTION            1
+             20 RAISE_VARARGS            1
 """ % (bug1333982.__code__.co_firstlineno + 1,
+       __file__,
+       bug1333982.__code__.co_firstlineno + 1,
        bug1333982.__code__.co_firstlineno + 2,
        bug1333982.__code__.co_firstlineno + 1)
-
 
 def bug42562():
     pass
@@ -436,14 +430,11 @@ async def _co(x):
     async for item in _ag(x):
         pass
 
-_h_str = """
 def _h(y):
     def foo(x):
         '''funcdoc'''
         return [x + z for z in y]
     return foo
-"""
-_h = compile_and_get(_h_str, "_h")
 
 dis_nested_0 = """\
 %3d           0 LOAD_CLOSURE             0 (y)
@@ -463,24 +454,40 @@ dis_nested_0 = """\
 
 dis_nested_1 = """%s
 Disassembly of <code object foo at 0x..., file "%s", line %d>:
-%3d           0 BUILD_LIST               0
-              2 LOAD_DEREF               0 (y)
-              4 GET_ITER
-        >>    6 FOR_ITER                 6 (to 20)
-              8 STORE_FAST               1 (z)
-             10 LOAD_FAST                0 (x)
-             12 LOAD_FAST                1 (z)
-             14 BINARY_ADD
-             16 LIST_APPEND              2
-             18 JUMP_ABSOLUTE            3 (to 6)
-        >>   20 DELETE_FAST              1 (z)
-             22 RETURN_VALUE
+%3d           0 LOAD_CLOSURE             0 (x)
+              2 BUILD_TUPLE              1
+              4 LOAD_CONST               1 (<code object <listcomp> at 0x..., file "%s", line %d>)
+              6 LOAD_CONST               2 ('_h.<locals>.foo.<locals>.<listcomp>')
+              8 MAKE_FUNCTION            8 (closure)
+             10 LOAD_DEREF               1 (y)
+             12 GET_ITER
+             14 CALL_FUNCTION            1
+             16 RETURN_VALUE
 """ % (dis_nested_0,
        __file__,
        _h.__code__.co_firstlineno + 1,
        _h.__code__.co_firstlineno + 3,
+       __file__,
+       _h.__code__.co_firstlineno + 3,
 )
 
+dis_nested_2 = """%s
+Disassembly of <code object <listcomp> at 0x..., file "%s", line %d>:
+%3d           0 BUILD_LIST               0
+              2 LOAD_FAST                0 (.0)
+        >>    4 FOR_ITER                 6 (to 18)
+              6 STORE_FAST               1 (z)
+              8 LOAD_DEREF               0 (x)
+             10 LOAD_FAST                1 (z)
+             12 BINARY_ADD
+             14 LIST_APPEND              2
+             16 JUMP_ABSOLUTE            2 (to 4)
+        >>   18 RETURN_VALUE
+""" % (dis_nested_1,
+       __file__,
+       _h.__code__.co_firstlineno + 3,
+       _h.__code__.co_firstlineno + 3,
+)
 
 class DisTests(unittest.TestCase):
 
@@ -687,6 +694,11 @@ class DisTests(unittest.TestCase):
 
         check(dis_nested_0, depth=0)
         check(dis_nested_1, depth=1)
+        check(dis_nested_2, depth=2)
+        check(dis_nested_2, depth=3)
+        check(dis_nested_2, depth=None)
+        check(dis_nested_2)
+
 
 
 class DisWithFileTests(DisTests):
