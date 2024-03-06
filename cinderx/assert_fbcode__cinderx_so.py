e351f023ccaebf04f801d9ba9eb9784ecdf3733d
Jacob Bower <jbower@meta.com>
2024-03-05 20:39:58 -0800

    Make default/dev Buck modes work
    Summary:
    In dev/default mode Buck builds all intermediate `cpp_library()`s as `.so`s. These then fail to link together as we don't declare our library dependencies, and can't do this until we get rid of circular dependencies.
    
    Something I don't fully understand is `assert_fbcode__cinderx_so` now seems to get `_cinderx` from the `.so` rather than from in the local binary via Native Python. This isn't something I'm testing for right now though.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D54288047
    
    fbshipit-source-id: 4ee8942768dfe0f4a7e21b1a411394cff8a54d22

--- cinderx/assert_fbcode__cinderx_so.py
+++ cinderx/assert_fbcode__cinderx_so.py
@@ -1,20 +1,24 @@
 import sys
 
 # This is not automatically loaded during startup, but we still want to check
-# it's loaded from fbcode (and so is a static-extension).
+# it's loaded from fbcode.
 import _strictmodule  # noqa: unused
 
 
-def assert_is_static_extension(module: str) -> None:
+def assert_is_from_fbcode(module: str) -> None:
     print(f"{module}: {sys.modules[module]}")
-    if str(sys.modules[module]) != f"<module '{module}' (static-extension)>":
-        raise AssertionError(f"{module} is not from fbcode")
+    mod_str = str(sys.modules[module])
+    if mod_str == f"<module '{module}' (static-extension)>":
+        return
+    elif "buck-out/" in mod_str:
+        return
+    raise AssertionError(f"{module} is not from fbcode")
 
 
 def main() -> None:
-    assert_is_static_extension("_cinderx")
-    assert_is_static_extension("_strictmodule")
-    assert_is_static_extension("_static")
+    assert_is_from_fbcode("_cinderx")
+    assert_is_from_fbcode("_strictmodule")
+    assert_is_from_fbcode("_static")
 
 
 if __name__ == '__main__':
