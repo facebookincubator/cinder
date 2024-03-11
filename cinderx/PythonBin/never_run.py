a61f5096845de94a03d877c073340c20a0c37b2c
Jacob Bower <jbower@meta.com>
2024-03-11 15:09:00 -0700

    Make Buck "install" of Python with CinderX from fbcode
    Reviewed By: alexmalyshev
    
    Differential Revision: D54720498
    
    fbshipit-source-id: 1dba88d6afb16b1471425f8a37d73fcd9e08ed16

--- cinderx/PythonBin/never_run.py
+++
@@ -1,4 +0,0 @@
-# This dummy Python file is just here the coax the Buck Native Python build
-# into making a binary we can run.
-
-raise Exception("This should never be run")
--- cinderx/PythonBin/test_python3.10_bin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-OUTPUT=$(mktemp)
-buck2 --isolation-dir test-python3.10-bin-$1 run @fbcode//mode/$1 fbcode//cinderx/PythonBin:python3.10 -- -c 'print("test")' | tee $OUTPUT
-[[ $(tail -1 $OUTPUT) = "test" ]] || exit 1
