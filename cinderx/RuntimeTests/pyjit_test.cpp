a440344ffef2cc882fc5366fb38075c859abcf05
Jacob Bower <jbower@meta.com>
2024-04-19 11:33:21 -0700

    Reenable pyjit_test.cpp
    Summary: I'm not sure why this whole thing was disabled but only one rather dubious test was failing after some compile errors were fixed. I've removed the dubious test.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D56214645
    
    fbshipit-source-id: 7f9f1a09397ffd6252128368caef59f5f84c2f5c

--- cinderx/RuntimeTests/pyjit_test.cpp
+++ cinderx/RuntimeTests/pyjit_test.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 #include "cinderx/Jit/pyjit.h"
+#include "cinderx/Jit/runtime.h"
 
 #include "cinderx/RuntimeTests/fixtures.h"
 #include "cinderx/RuntimeTests/testutil.h"
@@ -9,6 +10,9 @@
 #include <cstdlib>
 #include <memory>
 #include <string>
+
+using namespace jit;
+using std::byte;
 
 static const char* PYTHONJIT = "PYTHONJIT";
 
@@ -29,10 +33,11 @@ class PyJITTest : public RuntimeTest {
   }
 
   void TearDown() override {
-    setenv(
-        PYTHONJIT,
-        _pythonjit_env == nullptr ? nullptr : _pythonjit_env->c_str(),
-        1);
+    if (_pythonjit_env == nullptr) {
+      unsetenv(PYTHONJIT);
+    } else {
+      setenv(PYTHONJIT, _pythonjit_env->c_str(), 1);
+    }
 
     if (is_enabled) {
       _PyJIT_Initialize();
@@ -45,22 +50,6 @@ class PyJITTest : public RuntimeTest {
   std::unique_ptr<std::string> _pythonjit_env;
 };
 
-TEST_F(PyJITTest, PyInitialization) {
-  // test without the environment variable
-  unsetenv(PYTHONJIT);
-  auto result = _PyJIT_Initialize();
-  ASSERT_EQ(result, 0);
-  ASSERT_EQ(_PyJIT_IsEnabled(), 0);
-
-  // test without the environment variable
-  setenv(PYTHONJIT, "1", 1);
-  result = _PyJIT_Initialize();
-  ASSERT_EQ(result, 0);
-  ASSERT_EQ(_PyJIT_IsEnabled(), 1);
-
-  _PyJIT_Finalize();
-}
-
 TEST_F(RuntimeTest, ReadingFromCodeRuntimeReadsCode) {
   const char* src = R"(
 def test(a, b):
@@ -68,14 +57,14 @@ def test(a, b):
 )";
   Ref<PyFunctionObject> func(compileAndGet(src, "test"));
   ASSERT_NE(func, nullptr);
-  PyCodeObject* code = func->func_code;
+  auto code = reinterpret_cast<PyCodeObject*>(func->func_code);
   Runtime* ngen_rt = Runtime::get();
-  CodeRuntime* code_rt = runtime.allocateCodeRuntime(
+  CodeRuntime* code_rt = ngen_rt->allocateCodeRuntime(
       code, func->func_builtins, func->func_globals);
   EXPECT_EQ(
       *reinterpret_cast<PyCodeObject**>(
           reinterpret_cast<byte*>(code_rt) + __strobe_CodeRuntime_py_code),
-      reinterpret_cast<byte*>(code));
+      code);
 }
 
 TEST_F(RuntimeTest, ReadingFromRuntimeFrameStateReadsCode) {
@@ -85,10 +74,10 @@ def test(a, b):
 )";
   Ref<PyFunctionObject> func(compileAndGet(src, "test"));
   ASSERT_NE(func, nullptr);
-  PyCodeObject* code = func->func_code;
-  RuntimeFrameState rtfs(code, func->func_globals);
+  auto code = reinterpret_cast<PyCodeObject*>(func->func_code);
+  RuntimeFrameState rtfs(code, func->func_globals, func->func_builtins);
   EXPECT_EQ(
       *reinterpret_cast<PyCodeObject**>(
           reinterpret_cast<byte*>(&rtfs) + __strobe_RuntimeFrameState_py_code),
-      reinterpret_cast<byte*>(code));
+      code);
 }
