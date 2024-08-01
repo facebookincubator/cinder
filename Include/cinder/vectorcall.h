be270be6323fd2f24989232455d6a91556794e94
Jacob Bower <jbower@meta.com>
2024-07-31 19:42:01 -0700

    Move Ci_StaticFunction_Vectorcall and Ci_PyFunction_CallStatic to interpreter.h
    Summary: These aren't defined in the CPython runtime, they're our functions in our interpreter loop. We still need stubs for 3.12 but that's a different issue.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D60486929
    
    fbshipit-source-id: b4eb22acd68c1998063711453cd61499f0144638

--- Include/cinder/vectorcall.h
+++ Include/cinder/vectorcall.h
@@ -13,14 +13,3 @@
 /* Same as PyVectorcall_Call but allows passing extra flags to function being called */
 CiAPI_FUNC(PyObject *) Ci_PyVectorcall_Call_WithFlags(
     PyObject *callable, PyObject *tuple, PyObject *kwargs, size_t flags);
-
-
-CiAPI_FUNC(PyObject *)
-Ci_StaticFunction_Vectorcall(PyObject *func, PyObject* const* stack,
-                       size_t nargsf, PyObject *kwnames);
-
-CiAPI_FUNC(PyObject *)
-Ci_PyFunction_CallStatic(PyFunctionObject *func,
-                                 PyObject* const* args,
-                                 Py_ssize_t nargsf,
-                                 PyObject *kwnames);
